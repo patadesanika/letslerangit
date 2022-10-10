012b9e9fe97d58dff233476889daca6211737cc0
sanika Patade <khushi.patade@gmail.com>
2022-10-10 22:17:32 +0530

    initial commit

---
+++ sum.py
@@ -0,0 +1,8 @@
+def sum():
+    num1=int(input("Enter the first number\n"))
+    num2=int(input("enter a second number\n"))
+    result=num1+num2
+    print("Addition of two numbers is:- ",result)
+if __name__=="__main__":
+    sum()
+    
